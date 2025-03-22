e647e9485a8fc7b1447ff3013bf81ede24ab1ea0
Felipe Muñoz <analista.felipe.m@gmail.com>
2025-03-22 03:06:59 -0300

    Clase 22 completada.

---
+++ 22_vueProgresivo/hola.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Texto de prueba de Hola.vue</p>
+        <div>{{ text }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: "Texto de un componente"
+            }
+        }
+    }
+</script>
+
+<style>
+    div {
+        color: red;
+    }
+</style>
